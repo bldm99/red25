c77b726af932ded27f09ca99f9af57a85c8db417
bldm99 <bladimir.chipana@tecsup.edu.pe>
2023-12-01 21:48:41 -0500

    hi1

--- api/main.py
+++ api/main.py
@@ -77,7 +77,160 @@ def recibir_datos():
         peli['movieId'] = peli['movieId'].astype('int')
         peli['rating'] = peli['rating'].astype('float32')
 
-        df_userselect = peli[peli['userId'] == numerox]
+
+        def readLargeFile( data):
+            #data = pd.read_csv(filename, delimiter=delim, header=None)
+            data = data
+            lst = {} # Dictionary
+            j = 0
+            for index, row in data.iterrows():
+                #print(row[0], row[1], row[2])
+                if j != row[0]:
+                    j = row[0]
+                    tmp = {row[1]:row[2]}
+                    lst[row[0]] = tmp
+                else:
+                    tmp = lst.get(row[0])
+                    tmp[row[1]] = row[2]
+                    lst[row[0]] = tmp
+            return lst
+        
+                # Manhattan distance
+        def manhattanL(user1, user2):
+            dist = 0.0
+            count = 0
+            for i in user2:
+                if not (user1.get(i) is None):
+                    x = user1[i]
+                    y = user2[i]
+                    dist += abs(x - y)
+                    count += 1
+
+            if count == 0:
+                return 9999.99
+            return dist
+        
+                # Init K-vector with correct value based on distance type
+        def initVectDist(funName, N):
+            if funName == 'euclidiana' or funName == 'manhattan' or funName == 'euclidianaL' or funName == 'manhattanL':
+                ls = [99999] * N
+            else:
+                ls = [-1] * N
+
+            lu = [None] * N
+            return ls, lu
+
+
+        # Keep the closest values, avoiding sort
+        def keepClosest(funname, lstdist, lstuser, newdist, newuser, N):
+            if funname == 'euclidiana' or funname == 'manhattan' or funname == 'euclidianaL' or funname == 'manhattanL':
+                count = -1
+                for i in lstdist:
+                    count += 1
+                    if newdist > i:
+                        continue
+                    lstdist.insert(count, newdist)
+                    lstuser.insert(count, newuser)
+                    break
+            else:
+                count = -1
+                for i in lstdist:
+                    count += 1
+                    if newdist < i:
+                        continue
+                    lstdist.insert(count, newdist)
+                    lstuser.insert(count, newuser)
+                    break
+
+            if len(lstdist) > N:
+                lstdist.pop()
+                lstuser.pop()
+            return lstdist, lstuser
+        
+                # K-Nearest neighbour
+        def knn_L(N, distancia, usuario, data):  # N numero de vecinos
+            funName = distancia.__name__
+            print('k-nn', funName)
+
+            listDist, listName = initVectDist(funName, N)
+            nsize = len(data)
+            otherusers = range(0, nsize)
+            vectoruser = data.get(usuario)
+
+            for i in range(0, nsize):
+                tmpuser = i
+                if tmpuser != usuario:
+                    tmpvector = data.get(tmpuser)
+                    if not (tmpvector is None):
+                        tmpdist = distancia(vectoruser, tmpvector)
+                        if tmpdist is not math.nan:
+                            listDist, listName = keepClosest(funName, listDist, listName, tmpdist, tmpuser, N)
+
+            return listDist, listName
+        
+        def topSuggestions(fullObj, k, items):
+            rp = [-1]*items
+
+            for i in fullObj:
+                rating = fullObj.get(i)
+
+                for j in range(0, items):
+                    if rp[j] == -1 :
+                        tmp = [i, rating[0], rating[1]]
+                        rp.insert(j, tmp)
+                        rp.pop()
+                        break
+                    else:
+                        tval = rp[j]
+                        if tval[1] < rating[0]:
+                            tmp = [i, rating[0], rating[1]]
+                            rp.insert(j, tmp)
+                            rp.pop()
+                            break
+
+            return rp
+        
+        def recommendationL(usuario, distancia, N, items, minr, data):
+            ldistK, luserK = knn_L(N, distancia, usuario, data)
+
+            user = data.get(usuario)
+            recom = [None] * N
+            for i in range(0, N):
+                recom[i] = data.get(luserK[i])
+            # print('user preference:', user)
+
+            lstRecomm = [-1] * items
+            lstUser = [None] * items
+            lstObj = [None] * items
+            k = 0
+
+            fullObjs = {}
+            count = 0
+            for i in recom:
+                for j in i:
+                    tmp = fullObjs.get(j)
+                    if tmp is None:
+                        fullObjs[j] = [i.get(j), luserK[count]]
+                    else:
+                        nval = i.get(j)
+                        if nval > tmp[0]:
+                            fullObjs[j] = [nval, luserK[count]]
+                count += 1
+
+            finallst = topSuggestions(fullObjs, count, items)
+            return finallst
+        
+        lsrae = readLargeFile(rae.head(1000)) 
+        usuario = 45700
+        rfunc = manhattanL
+
+        # 10 vecinos, 20 recomendaciones
+        lista = recommendationL(usuario, rfunc, 10, 20, 3.0, lsrae) # 3 seg
+
+
+
+
+        '''df_userselect = peli[peli['userId'] == numerox]
         movie_ids_user1 = df_userselect['movieId'].tolist()
         rae = peli.query('movieId in @movie_ids_user1')
 
@@ -136,7 +289,7 @@ def recibir_datos():
 
         datarecomend = dict(lista_tuplas)
         peliculasp = datarecomend
-
+'''
         
     
 
